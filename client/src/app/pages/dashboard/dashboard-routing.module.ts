7217d1760e2c9cc4b55ed88a992737cbf7253d9f
Jose Pastor <jlpastor@mensoft.es>
2022-05-26 15:19:40 +0200

    Visual Login + Pages

--- client/src/app/pages/dashboard/dashboard-routing.module.ts
+++ client/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -10,13 +10,20 @@ const routes: Routes = [
         loadChildren: () => import('./emit/emit.module').then(m => m.EmitPageModule)
       },
       {
+        path: 'customers',
+        loadChildren: () => import('./customers/customers.module').then(m => m.CustomersPageModule)
+      },
+      {
+        path: 'invoices',
+        loadChildren: () => import('./invoices/invoices.module').then(m => m.InvoicesPageModule)
+      },
+      {
         path: '',
         pathMatch: 'full',
         redirectTo: 'emit'
       }
     ]
-  },
-
+  }
 ];
 
 @NgModule({
