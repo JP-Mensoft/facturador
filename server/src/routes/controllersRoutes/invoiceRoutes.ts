d4a102ba84ed3b7c8ae964bf3dfde32ffdf9582d
JP-Mensoft <jlpastor@mensoft.es>
2022-05-31 19:57:19 +0200

    Update invoiceRoutes.ts

--- server/src/routes/controllersRoutes/invoiceRoutes.ts
+++ server/src/routes/controllersRoutes/invoiceRoutes.ts
@@ -2,6 +2,20 @@ import { InvoiceController } from "../../controllers/invoiceController";
 
 export const invoiceRoutes = [
     {
+        method: 'get',
+        route: '/invoice/getone',
+        middlewares: 'checkToken',
+        controller: InvoiceController,
+        action: 'getOneInvoice'
+    },
+    {
+        method: 'get',
+        route: '/invoice/getall',
+        middlewares: 'checkToken',
+        controller: InvoiceController,
+        action: 'getAllInvoices'
+    },
+    {
         method: 'post',
         route: '/invoice/add',
         middlewares: 'checkToken',
